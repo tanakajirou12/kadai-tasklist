19c8ca0d808018d2b7ddc62c9fec73e9785c1618
jirou.tanaka <tanakajiroumaru12@gmail.com>
2020-03-31 08:12:02 +0000

    kadai-tasklist

--- database/migrations/2020_03_31_143156_add_status_to_tasks_table.php
+++ database/migrations/2020_03_31_143156_add_status_to_tasks_table.php
@@ -14,8 +14,7 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status');
-            $table->char('VARCHAR', 10);
+            $table->string('status', 10);
         });
     }
 
